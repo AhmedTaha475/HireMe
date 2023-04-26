b03d9ede5935eca9aa61c3a7e779104672c7d8fd
Maha Yehia <maha5yehia@gmail.com>
2023-04-26 07:00:02 +0200

    add freelancer profile

--- src/app/Components/FreelancerProfile/freelancer-profile/freelancer-profile.component.ts
+++ src/app/Components/FreelancerProfile/freelancer-profile/freelancer-profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LookupValueService } from 'src/app/Services/LookupValues_Service/lookup-value.service';
 import { FreelancerService } from 'src/app/Services/freelancer.service';
+import{StaticHelper} from 'src/app/Helpers/static-helper'
+import { TranslateCompiler, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-freelancer-profile',
@@ -10,16 +13,28 @@ import { FreelancerService } from 'src/app/Services/freelancer.service';
 export class FreelancerProfileComponent implements OnInit  {
 id:any;
 myFreelancer:any ;
-  constructor(public myActiveRoute:ActivatedRoute , public freelancer:FreelancerService) {
-    this.id=myActiveRoute.snapshot.params["Id"]
-  }
 
+
+  constructor(public myActiveRoute:ActivatedRoute , public freelancer:FreelancerService , public translate: TranslateService) {
+    this.id=myActiveRoute.snapshot.params["Id"]
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
   ngOnInit(): void {
    this.freelancer.GetCurrentFreelancer().subscribe({
-    next:(data)=>{this.myFreelancer=data},
+    next:(data:any)=>{this.myFreelancer=data.body;
+      this.myFreelancer.cv=StaticHelper.ConvertByteArrayToPdf(data.body.cv);
+      this.myFreelancer.image=StaticHelper.ConvertByteArrayToImage(data.body.image);
+
+       console.log(data)},
     error:(err)=>{console.log(err)}
    })
 
+
   }
+
 
 }
