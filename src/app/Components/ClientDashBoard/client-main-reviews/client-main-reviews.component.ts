f2d9ae00daa3292ad3514339862168f9049a68c6
AhmedTaha475 <76524115+AhmedTaha475@users.noreply.github.com>
2023-05-09 23:08:54 +0300

    AddedReview

---
+++ src/app/Components/ClientDashBoard/client-main-reviews/client-main-reviews.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/app/Services/client.service';
+import { FreelancerService } from 'src/app/Services/freelancer.service';
+import { ProjectReviewService } from 'src/app/Services/project-review.service';
+
+@Component({
+  selector: 'app-client-main-reviews',
+  templateUrl: './client-main-reviews.component.html',
+  styleUrls: ['./client-main-reviews.component.css'],
+})
+export class ClientMainReviewsComponent implements OnInit {
+  ReviewsObject: any[] = [];
+  MyCustomData: number = 5;
+  reviewList: any[] = [];
+  freelancerData: any;
+  currentClientId: any;
+  constructor(
+    private freelancerServ: FreelancerService,
+    private reviewServ: ProjectReviewService,
+    private clientServ: ClientService
+  ) {}
+  ngOnInit(): void {
+    this.getcurrentclient();
+  }
+  private getcurrentclient() {
+    this.clientServ.GetCurrentClient().subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.currentClientId = data.body.id;
+        this.getallreviews();
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
+  }
+  private getallreviews() {
+    this.reviewServ.GetAllClientReviews(this.currentClientId).subscribe({
+      next: (data: any) => {
+        this.reviewList = data;
+        console.log(data);
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
+  }
+  private getFreelancerDataWithReviews() {
+    this.ReviewsObject = this.reviewList.map((el: any) => {});
+  }
+}
